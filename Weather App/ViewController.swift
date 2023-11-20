d799e12a809c9e5e1e9df4efe5260c1e5dfa0d78
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2023-11-20 12:18:06 -0400

    AULA 03 - Customizando componentes do UIKit

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -8,52 +8,117 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    private lazy var customView: UIView = {
-            let view = UIView(frame: .zero)
-            view.backgroundColor = .black
-            view.translatesAutoresizingMaskIntoConstraints = false
-            return view
-        }()
+    
+    private lazy var backgroundView: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.image = UIImage(named: "background")
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var headerView: UIView = {
+        let view = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 20
+        return view
+    }()
+    
+    private lazy var cityLabel: UILabel = {
+        let cityLabel = UILabel()
+        cityLabel.translatesAutoresizingMaskIntoConstraints = false
+        cityLabel.font = UIFont.systemFont(ofSize: 20)
+        cityLabel.text = "São Paulo"
+        cityLabel.textColor = UIColor(named: "primaryColor")
+        cityLabel.textAlignment = .center
+        return cityLabel
+    }()
+    
+    private lazy var temperatureLabel : UILabel = {
+        let tempLabel = UILabel()
+        tempLabel.translatesAutoresizingMaskIntoConstraints = false
+        tempLabel.font = UIFont.systemFont(ofSize: 70, weight: .bold)
+        tempLabel.textAlignment = .left
+        tempLabel.text = "25°C"
+        tempLabel.textColor = UIColor(named: "primaryColor")
+        return tempLabel
+    }()
+    
+    private lazy var weatherIcon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "sunIcon")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    private func setupView() {
         
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            setupView()
-        }
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        view.addSubview(backgroundView)
+        view.addSubview(headerView)
         
-        override func viewDidAppear(_ animated: Bool) {
-            super.viewDidAppear(animated)
-        }
+        headerView.addSubview(cityLabel)
+        headerView.addSubview(temperatureLabel)
+        headerView.addSubview(weatherIcon)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
         
-        override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-        }
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 35),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -35),
+            headerView.heightAnchor.constraint(equalToConstant: 169)
+        ])
         
-        override func viewWillDisappear(_ animated: Bool) {
-            super.viewWillDisappear(animated)
-        }
+        NSLayoutConstraint.activate([
+            cityLabel.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 15),
+            cityLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 15),
+            cityLabel.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -15),
+            cityLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            temperatureLabel.topAnchor.constraint(equalTo: cityLabel.bottomAnchor, constant: 21),
+            temperatureLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 26),
+            
+            weatherIcon.heightAnchor.constraint(equalToConstant: 86),
+            weatherIcon.widthAnchor.constraint(equalToConstant: 86),
+            weatherIcon.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -26),
+            weatherIcon.centerYAnchor.constraint(equalTo: temperatureLabel.centerYAnchor),
+            weatherIcon.leadingAnchor.constraint(equalTo: temperatureLabel.trailingAnchor, constant: 15)
+        ])
         
-        override func viewDidDisappear(_ animated: Bool) {
-            super.viewDidDisappear(animated)
-        }
-        
-        private func setupView() {
-            view.backgroundColor = .red
-
-            setHierarchy()
-            setConstraints()
-        }
-        
-        private func setHierarchy() {
-            view.addSubview(customView)
-        }
-        
-        private func setConstraints() {
-            NSLayoutConstraint.activate([
-                customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
-                customView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
-                customView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
-                customView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100)
-            ])
-        }
+    }
 }
